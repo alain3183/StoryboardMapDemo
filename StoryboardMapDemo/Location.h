effd11de5451ae54d11f84e656448615e03c5391
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-26 20:55:03 -0500

    Add a class named "Location", subclass of "NSObject"

---
+++ StoryboardMapDemo/Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  StoryboardMapDemo
+//
+//  Created by Alain Malenfant on 11/26/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
---
+++ StoryboardMapDemo/Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  StoryboardMapDemo
+//
+//  Created by Alain Malenfant on 11/26/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
