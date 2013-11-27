0cf243d84692916e2c5b3f87d9d36f48138ea381
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-26 21:49:25 -0500

    VC.m

--- StoryboardMapDemo/ViewController.m
+++ StoryboardMapDemo/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "LocationDataController.h"
+#import "Location.h"
 
 @interface ViewController ()
 
@@ -17,7 +19,16 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    LocationDataController *model = [[LocationDataController alloc] init];
+    Location *poi = [model getPointOfInterest];
+    
+    self.addressLabel.text = poi.address;
+    [self.photoImageView setImage:[UIImage imageNamed:poi.photoFileName]];
 }
 
 - (void)didReceiveMemoryWarning
