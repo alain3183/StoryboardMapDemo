a887a6f82b2fa0dd6cdb82d0f9a964d7ae1f48d2
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-26 20:58:30 -0500

    Add a class named "LocationDataController”, subclass of "NSObject"

---
+++ StoryboardMapDemo/LocationDataController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDataController.h
+//  StoryboardMapDemo
+//
+//  Created by Alain Malenfant on 11/26/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationDataController : NSObject
+
+@end
---
+++ StoryboardMapDemo/LocationDataController.m
@@ -0,0 +1,13 @@
+//
+//  LocationDataController.m
+//  StoryboardMapDemo
+//
+//  Created by Alain Malenfant on 11/26/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import "LocationDataController.h"
+
+@implementation LocationDataController
+
+@end
