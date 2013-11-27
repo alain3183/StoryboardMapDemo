fb0276a52c4c625eda0c4a850e480347fb1b639c
Alain Malenfant <alainmalenfant@yahoo.com>
2013-11-26 20:09:00 -0500

    Initial Commit

---
+++ StoryboardMapDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardMapDemo
+//
+//  Created by Alain Malenfant on 11/26/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoryboardMapDemoTests/StoryboardMapDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  StoryboardMapDemoTests.m
+//  StoryboardMapDemoTests
+//
+//  Created by Alain Malenfant on 11/26/2013.
+//  Copyright (c) 2013 Alain Malenfant. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StoryboardMapDemoTests : XCTestCase
+
+@end
+
+@implementation StoryboardMapDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
